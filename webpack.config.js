817ce9798ca18720191043404f4a1eede4c82f56
JGuevara23 <axndr154@gmail.com>
2025-06-23 21:49:04 -0500

    Creación de webpack.config.js y actualización del package.json

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    static: './dist',
+    open: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ]
+  },
+  mode: 'development'
+};
